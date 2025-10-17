5532171622551bf61410a4eec58f0bbf93bf0aeb
Kaleb Hirshfield <kaleb.hirshfield@protonmail.com>
2025-10-17 17:15:22 +0100

    feat: implement player movement and clamps

--- core/src/main/java/io/github/eng1team3/Main.java
+++ core/src/main/java/io/github/eng1team3/Main.java
@@ -1,28 +1,58 @@
 package io.github.eng1team3;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image;
     private FitViewport viewport;
+
+    private Texture playerFrontTexture;
+    private Texture playerBackTexture;
+    private Texture playerLeftTexture;
+    private Texture playerRightTexture;
+
+    private Sprite playerFrontSprite;
+    private Sprite playerBackSprite;
+    private Sprite playerLeftSprite;
+    private Sprite playerRightSprite;
+
+    private Sprite activeSprite;
+
+    private float startX;
+    private float startY;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-        viewport = new FitViewport(8, 5);
+        viewport = new FitViewport(16, 9);
+
+        playerFrontTexture = new Texture("FrontView.png");
+
+        playerFrontSprite = new Sprite(playerFrontTexture);
+
+        playerFrontSprite.setSize(1f, 1f);
+
+        startX = viewport.getWorldWidth() / 2f - playerFrontSprite.getWidth() / 2f;
+        startY = viewport.getWorldHeight() / 2f - playerFrontSprite.getHeight() / 2f;
+
+        playerFrontSprite.setPosition(startX, startY);
+
+        activeSprite = playerFrontSprite;
     }
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height, true); // true centers the camera
+        viewport.update(width, height, true);
     }
 
     @Override
@@ -33,11 +63,29 @@ public class Main extends ApplicationAdapter {
     }
 
     private void input() {
+        float speed = 4f;
+        float delta = Gdx.graphics.getDeltaTime();
 
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            activeSprite.translateX(speed * delta);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            activeSprite.translateX(-speed * delta); // move the bucket left
+        } else if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            activeSprite.translateY(speed * delta); // move the bucket left
+        } else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            activeSprite.translateY(-speed * delta); // move the bucket left
+        }
     }
 
     private void logic() {
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
 
+        float playerWidth = activeSprite.getWidth();
+        float playerHeight = activeSprite.getHeight();
+
+        playerFrontSprite.setX(MathUtils.clamp(playerFrontSprite.getX(), 0, worldWidth -  playerWidth));
+        playerFrontSprite.setX(MathUtils.clamp(playerFrontSprite.getX(), 0, worldWidth -  playerWidth));
     }
 
     private void draw() {
@@ -46,12 +94,16 @@ public class Main extends ApplicationAdapter {
         batch.setProjectionMatrix(viewport.getCamera().combined);
         batch.begin();
 
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+        playerFrontSprite.draw(batch);
+
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        playerFrontTexture.dispose();
     }
 }
