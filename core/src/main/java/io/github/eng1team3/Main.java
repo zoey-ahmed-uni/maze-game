520ee26739865a60f5e46abbfda42cd3260f08ca
Kaleb Hirshfield <kaleb.hirshfield@protonmail.com>
2025-10-17 15:09:59 +0100

    feat: display viewport with a projection matrix

--- core/src/main/java/io/github/eng1team3/Main.java
+++ core/src/main/java/io/github/eng1team3/Main.java
@@ -1,26 +1,51 @@
 package io.github.eng1team3;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private FitViewport viewport;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+        viewport = new FitViewport(8, 5);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true); // true centers the camera
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        input();
+        logic();
+        draw();
+    }
+
+    private void input() {
+
+    }
+
+    private void logic() {
+
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        batch.setProjectionMatrix(viewport.getCamera().combined);
         batch.begin();
-        batch.draw(image, 140, 210);
+
         batch.end();
     }
 
