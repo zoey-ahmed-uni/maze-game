4b5d1ecb25e204e60d97fe02dd77629376d1a50f
Max Emmett <maxemmett15@gmail.com>
2025-10-25 00:06:13 +0100

    style: JavaDoc/Comments added
    Added JavaDoc and Comments. Also added in a constant for Window Width and Height

--- core/src/main/java/io/github/eng1team3/Main.java
+++ core/src/main/java/io/github/eng1team3/Main.java
@@ -14,29 +14,77 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
+/**
+ * The Main Game
+ * pass this into any screen you intend on adding to access
+ * the spritebatch
+ */
 public class Main extends Game {
 
+    //Remember to change these 2 constants if messing with the window size in the config files
+    //to avoid graphical issues
+    private static final int WIDTH = 640;
+    private static final int HEIGHT = 480;
     private SpriteBatch batch;
 
+    /**
+     * called when application is first created.
+     *
+     * creates the spritebatch to be used by all screens
+     * rather than creating a new one for each screen which
+     * will increase GPU load and decrease performance
+     * */
     @Override
     public void create(){
         batch = new SpriteBatch();
-        this.setScreen(new GameScreen(this));
+        this.setScreen(new MainMenu(this));
     }
-
+    /**
+     * Called when the application should render itself.
+     * allows screen to update and draw things.
+     * ApplicationListener already has render implemented
+     * so we can just make a super call to let it handle this
+     * */
     @Override
     public void render(){
         super.render();
     }
 
+    /**
+     * Get sprite batch.
+     *
+     * @return the sprite batch
+     */
     public SpriteBatch getBatch(){
         return this.batch;
     }
 
+    /**
+     * Sets batch.
+     *
+     * @param batch the batch
+     */
     public void setBatch(SpriteBatch batch) {
         this.batch = batch;
     }
 
+    /**
+     * Get width int.
+     *
+     * @return the width
+     */
+    public int getWidth(){
+        return WIDTH;
+    }
+
+    /**
+     * Get height int.
+     *
+     * @return the height
+     */
+    public int getHeight(){
+        return HEIGHT;
+    }
 
 
 
