52e10fb74488d4a3ebe24aa972a989e2895fa1f4
alex-pdl <apodlesny00@gmail.com>
2025-11-06 12:43:17 +0000

    docs: Added documentation to class Player

--- core/src/main/java/io/maze/entities/Player.java
+++ core/src/main/java/io/maze/entities/Player.java
@@ -1,5 +1,8 @@
 package io.maze.entities;
 
+/**
+ * The Player class represents the character that the player is manipulating.
+ */
 public class Player extends Entity {
 
     private float spawnX;
@@ -18,11 +21,18 @@ public class Player extends Entity {
         this.setSpawnPoint(this.getX(), this.getY());
     }
 
+    /**
+     * Sets the spawn position of the character at the start of the game.
+     *
+     * @param x the starting x coordinate
+     * @param y the starting y coordinate
+     */
     public void setSpawnPoint(float x, float y){
         this.spawnX = x;
         this.spawnY = y;
     }
 
+    /** Sets the player character position back to its spawn coordinates. */
     public void respawn(){
         this.setX(spawnX);
         this.setY(spawnY);
